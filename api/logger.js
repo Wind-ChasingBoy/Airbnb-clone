82c1276c9d80f289b45c6008bd0b25a57d227601
redearly123 <1900012936@pku.edu.cn>
2024-03-01 19:04:38 +0800

    helmet+validator+env_variable+winston_log

---
+++ api/logger.js
@@ -0,0 +1,34 @@
+const winston = require('winston');
+
+const logLevels = {
+    error: 0,
+    warn: 1,
+    info: 2,
+    debug: 3,
+};
+
+const logColors = {
+    error: 'red',
+    warn: 'yellow',
+    info: 'green',
+    debug: 'blue',
+};
+
+const logger = winston.createLogger({
+    levels: logLevels,
+    format: winston.format.simple(),
+    transports: [
+        new winston.transports.Console({
+            format: winston.format.combine(
+                winston.format.colorize(),
+                winston.format.simple()
+            ),
+        }),
+        new winston.transports.File({ filename: 'error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'combined.log' }),
+    ],
+});
+
+winston.addColors(logColors);
+
+module.exports = logger;
